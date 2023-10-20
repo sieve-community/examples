4aaef7e7a3fa9fa5d3c852c3d1d3c79517f511bf
Gaurang Bharti <gaurangbharti@gmail.com>
2023-10-20 11:02:08 +0530

    Delete custom_text_to_speech directory

--- custom_text_to_speech/wf.py
+++
@@ -1,6 +0,0 @@
-import sieve
-from main import TortoiseTTS
-
-@sieve.workflow(name="custom_tortoise_tts")
-def tortoise_tts(text: str, preset: str, audio1: sieve.Audio, audio2: sieve.Audio) -> sieve.Audio:
-    return TortoiseTTS()(text, preset, audio1, audio2)

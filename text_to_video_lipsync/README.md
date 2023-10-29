9d81a2283b63aac4a17e0340715f954f2d6c56fe
Mokshith Voodarla <mvoodarla@gmail.com>
2023-10-29 12:36:18 -0700

    feat: alpha text to video lipsync

---
+++ text_to_video_lipsync/README.md
@@ -0,0 +1,21 @@
+# Text to Video Lipsyncing
+
+**NOTE: This app is currently in alpha, so please expect some bugs and quality issues.**
+
+This app takes in a video file and a piece of text and outputs a video that makes it look like the person in the video is saying the text.
+
+For generating the text, we give the user an option to pick between the open source [xtts](https://www.sievedata.com/functions/sieve/xtts-v1) or the [ElevenLabs](https://www.sievedata.com/functions/sieve/elevenlabs_speech_synthesis) Text-to-Speech models. The ElevenLabs model is recommended for better quality but requires an ElevenLabs API key.
+
+If you are using the ElevenLabs model, you can also choose to either clone the voice from the audio within the video itself or use a `voice_id` that you've already either created or is available in ElevenLabs. If you choose to clone the voice, you can also choose to delete the voice after use.
+
+## Options
+
+- `source_video`: The video file to lip-sync.
+- `text`: The text that the person in the video will appear to speak.
+- `tts_model`: The Text-to-Speech model to use. Supported models are "xtts" and "elevenlabs". "elevenlabs" is recommended for better quality but requires an ElevenLabs API key.
+- `speech_stability`: A value between 0 and 1. Increasing this value can make the speech more expressive with output varying between re-generations. However, it can also lead to instabilities.
+- `speech_similarity_boost`: A value between 0 and 1. Lower values are recommended if there are background artifacts present in the generated speech.
+- `elevenlabs_voice_id`: The ID of the ElevenLabs voice to use. If none are set, the voice will be cloned from the source audio and used. This is only applicable if the `tts_model` is set to "elevenlabs".
+- `elevenlabs_cleanup_voice_id`: Whether to delete the ElevenLabs voice after use. This is only applicable if the `tts_model` is set to "elevenlabs".
+- `refine_source_audio`: Whether to refine the source audio using sieve/audio_enhancement.
+- `refine_target_audio`: Whether to refine the generated target audio using sieve/audio_enhancement.
---
+++ text_to_video_lipsync/main.py
@@ -0,0 +1,184 @@
+import sieve
+
+metadata = sieve.Metadata(
+    description="Generate a video of a person speaking a given text using lipsyncing.",
+    code_url="https://github.com/sieve-community/examples/blob/main/text_to_video_lipsync",
+    image=sieve.Image(
+        url="https://storage.googleapis.com/mango-public-models/dalle-lipsync-lego.png"
+    ),
+    tags=["Audio", "Speech", "TTS", "Voice Cloning", "Video", "Lipsyncing"],
+    readme=open("README.md", "r").read(),
+)
+
+@sieve.function(
+    name="text_to_video_lipsync",
+    system_packages=[
+        "ffmpeg",
+        "rubberband-cli"
+    ],
+    python_packages=[
+        "librosa",
+        "soundfile",
+        "moviepy",
+        "pydub",
+        "pyrubberband",
+    ],
+    metadata=metadata,
+)
+def do(
+    source_video: sieve.Video,
+    text: str,
+    tts_model: str = "xtts",
+    speech_stability: float = 0.5,
+    speech_similarity_boost: float = 0.63,
+    elevenlabs_voice_id: str = "",
+    elevenlabs_cleanup_voice_id: bool = False,
+    refine_source_audio: bool = True,
+    refine_target_audio: bool = True
+):
+    '''
+    :param source_video: video to lip-sync
+    :param text: text to speak
+    :param tts_model: TTS model to use. Supported models: "xtts" or "elevenlabs". "elevenlabs" is recommended for better quality but requires an ElevenLabs API key.
+    :param speech_stability: Value between 0 and 1. Increasing variability can make speech more expressive with output varying between re-generations. It can also lead to instabilities.
+    :param speech_similarity_boost: Value between 0 and 1. Low values are recommended if background artifacts are present in generated speech.
+    :param elevenlabs_voice_id: The ID of the ElevenLabs voice to use. If none are set, the voice will be cloned from the source audio and used. Only applicable if tts_model is "elevenlabs".
+    :param elevenlabs_cleanup_voice_id: Whether to delete the voice after use. Only applicable if tts_model is "elevenlabs".
+    :param refine_source_audio: Whether to refine the source audio using sieve/audio_enhancement.
+    :param refine_target_audio: Whether to refine the generated target audio using sieve/audio_enhancement.
+    :return: A generated video file
+    '''
+    import os
+    import time
+    from utils import extract_audio_from_video, trim_silence_from_video, trim_silence_from_audio_loaded, convert_to_format
+
+    source_video_path = source_video.path
+    source_audio_path = "source_audio.wav"
+    if os.path.exists(source_audio_path):
+        os.remove(source_audio_path)
+    
+    # Extract audio from source_video
+    extract_audio_from_video(source_video_path, source_audio_path)
+    source_audio = sieve.Audio(path=source_audio_path)
+
+    # Refine source_audio
+    if refine_source_audio:
+        start_time = time.time()
+        source_audio = sieve.function.get("sieve/audio_enhancement").run(source_audio, filter_type="all")
+        print(f"Time taken to refine source audio: {time.time() - start_time} seconds")    
+
+    # split text into sentences by punctuation
+    import re
+    segments = []
+    for sentence in re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', text):
+        segments.append({"text": sentence})
+    # TTS using audio as source
+    tts_model_str = tts_model
+    start_time = time.time()
+    target_audios = []
+    tts_coroutines = []
+    if tts_model_str  == "xtts":
+        tts_model = sieve.function.get(f"sieve/xtts-v1")
+        for i, segment in enumerate(segments):
+            tts = tts_model.push(
+                segment["text"],
+                source_audio,
+                stability=speech_stability,
+                similarity_boost=speech_similarity_boost
+            )
+            tts_coroutines.append(tts)
+    elif tts_model_str == "elevenlabs":
+        tts_model = sieve.function.get(f"sieve/elevenlabs_speech_synthesis")
+        if len(elevenlabs_voice_id) == 0:
+            # clone voice
+            cloning_model = sieve.function.get("sieve/elevenlabs_voice_cloning")
+            voice_cloning = cloning_model.run(source_audio)
+            elevenlabs_voice_id = voice_cloning["voice_id"]
+        for i, segment in enumerate(segments):
+            if elevenlabs_voice_id and len(elevenlabs_voice_id) > 0:
+                tts = tts_model.push(
+                    segment["text"],
+                    voice_id=elevenlabs_voice_id,
+                    stability=speech_stability,
+                    similarity_boost=speech_similarity_boost
+                )
+            else:
+                tts = tts_model.push(
+                    segment["text"],
+                    stability=speech_stability,
+                    similarity_boost=speech_similarity_boost
+                )
+                
+            tts_coroutines.append(tts)
+        
+        if elevenlabs_cleanup_voice_id:
+            # delete voice
+            cloning_model = sieve.function.get("sieve/elevenlabs_voice_cloning")
+            cloning_model.run(source_audio, delete_voice_id=elevenlabs_voice_id)
+    else:
+        raise ValueError(f"Unsupported TTS model: {tts_model}. Please use one of the following: xtts-v1, eleven_labs_voice_cloning")
+    for tts in tts_coroutines:
+        target_audios.append(tts.result())
+    print(f"Time taken for TTS: {time.time() - start_time} seconds")
+    
+    if refine_target_audio:
+        # Refine each target audio snippet
+        start_time = time.time()
+        refined_target_audios = []
+        audio_enhancement_coroutines = []
+        for target_audio in target_audios:
+            enhanced_audio = sieve.function.get("sieve/audio_enhancement").push(target_audio, filter_type="all")
+            audio_enhancement_coroutines.append(enhanced_audio)
+        for i, enhanced_audio in enumerate(audio_enhancement_coroutines):
+            try:
+                refined_target_audios.append(enhanced_audio.result())
+            except Exception as e:
+                print(f"Exception at index {i} of audio_enhancement_coroutines: {e}, using target_audios[{i}] instead.")
+                print(f"target audio is {target_audios[i]}")
+                refined_target_audios.append(target_audios[i])
+        print(f"Time taken to refine target audio: {time.time() - start_time} seconds")
+        target_audios = refined_target_audios
+    
+    # Combine target audios with gaps
+    from pydub import AudioSegment
+    combined_audio = AudioSegment.empty()
+    for i, target_audio in enumerate(target_audios):
+        # Trim silence from target_audio
+        start_time = time.time()
+        target_audio_path = target_audio.path
+        segment_audio = AudioSegment.from_wav(target_audio_path)
+        trimmed_audio = trim_silence_from_audio_loaded(segment_audio)
+        if i < len(segments) - 1:
+            try:
+                gap_duration = (segments[i+1]["start"] - segments[i]["words"][-1]["end"]) * 1000  # Convert to milliseconds
+            except KeyError:
+                print(f"KeyError at index {i} of segments. Using default gap duration of 0.05 seconds.")
+                gap_duration = 0.05 * 1000
+            gap = AudioSegment.silent(duration=gap_duration)
+            combined_audio += trimmed_audio + gap
+        else:
+            combined_audio += trimmed_audio
+    combined_audio.export("combined_audio.wav", format="wav")
+    target_audio = sieve.Audio(path="combined_audio.wav")
+
+    # Combine audio and video with Retalker
+    start_time = time.time()
+    retalker = sieve.function.get("sieve-internal/video_retalking")
+    output_video = retalker.run(source_video, target_audio)
+    print(f"Time taken to combine audio and video: {time.time() - start_time} seconds")
+
+    # Trim silence from output_video
+    output_video_path = output_video.path
+    if os.path.exists("output_video_trimmed.mp4"):
+        os.remove("output_video_trimmed.mp4")
+    start_time = time.time()
+    trim_silence_from_video(output_video_path, "output_video_trimmed.mp4")
+    print(f"Time taken to trim silence: {time.time() - start_time} seconds")
+
+    trimmed_video = sieve.Video(path="output_video_trimmed.mp4")
+    return trimmed_video
+
+
+
+    
+    

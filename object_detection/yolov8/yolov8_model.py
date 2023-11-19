a44ce435027eca1a1312cf33f5ff2cde6f2fa43a
Gaurang Bharti <gaurangbharti@gmail.com>
2023-11-20 04:14:17 +0530

    Yolov8 boxes for every frame (#43)
    Co-authored-by: Mokshith Voodarla <mvoodarla@users.noreply.github.com>

--- object_detection/yolov8/yolov8_model.py
+++ object_detection/yolov8/yolov8_model.py
@@ -1,27 +1,24 @@
 import sieve
 
 metadata = sieve.Metadata(
-    description = "Ultralytics YOLOv8, the latest version of the acclaimed real-time object detection model.",
-    code_url = "https://github.com/sieve-community/examples/blob/main/object_detection/yolov8",
+    description="Ultralytics YOLOv8, the latest version of the acclaimed real-time object detection model.",
+    code_url="https://github.com/sieve-community/examples/blob/main/object_detection/yolov8",
     tags=["Image", "Object", "Detection"],
     image=sieve.Image(
         url="https://www.freecodecamp.org/news/content/images/2023/04/compvision_tasks.png"
     ),
-    readme=open("README.md", "r").read()
+    readme=open("README.md", "r").read(),
 )
+
 
 @sieve.Model(
     name="yolov8l",
     gpu=True,
-    python_packages=[
-        'ultralytics',
-        'torch==1.13.1',
-        'torchvision==0.14.1'
-    ],
+    python_packages=["ultralytics", "torch==1.13.1", "torchvision==0.14.1"],
     cuda_version="11.7.1",
     system_packages=["libgl1-mesa-glx", "libglib2.0-0", "ffmpeg"],
     python_version="3.10",
-    metadata=metadata
+    metadata=metadata,
 )
 class YOLOv8:
     def __setup__(self):
@@ -41,8 +38,8 @@ class YOLOv8:
         import os
         import time
 
-        video_extensions = ['mp4', 'avi', 'mov', 'flv']
-        image_extensions = ['jpg', 'jpeg', 'png', 'bmp', 'tiff']
+        video_extensions = ["mp4", "avi", "mov", "flv"]
+        image_extensions = ["jpg", "jpeg", "png", "bmp", "tiff"]
 
         file_extension = os.path.splitext(file.path)[1][1:]
         if file_extension in video_extensions:
@@ -58,12 +55,11 @@ class YOLOv8:
                     else:
                         results = self.model(frame, classes=classes)
                     results_dict = self.__process_results__(results)
+                    results_dict["frame_number"] = count
+                    count += 1
                     # Filter results based on confidence threshold
                     results_dict["boxes"] = [box for box in results_dict["boxes"] if box["confidence"] > confidence_threshold]
-                    if results_dict["boxes"]:
-                        results_dict["frame_number"] = count
-                        count += 1
-                        yield results_dict
+                    yield results_dict
                 else:
                     break
 
@@ -78,12 +74,19 @@ class YOLOv8:
             else:
                 results = self.model(image_path, classes=classes)
             results_dict = self.__process_results__(results)
+
             # Filter results based on confidence threshold
-            results_dict["boxes"] = [box for box in results_dict["boxes"] if box["confidence"] > confidence_threshold]
+            results_dict["boxes"] = [
+                box
+                for box in results_dict["boxes"]
+                if box["confidence"] > confidence_threshold
+            ]
             if results_dict["boxes"]:
                 yield results_dict
         else:
-            raise ValueError(f"Unsupported file extension: {file_extension}. Supported extensions are {video_extensions + image_extensions}")
+            raise ValueError(
+                f"Unsupported file extension: {file_extension}. Supported extensions are {video_extensions + image_extensions}"
+            )
 
     def __process_results__(self, results) -> dict:
         results_dict = {
@@ -98,10 +101,12 @@ class YOLOv8:
                     "y1": box.xyxy.cpu().numpy().tolist()[0][1],
                     "x2": box.xyxy.cpu().numpy().tolist()[0][2],
                     "y2": box.xyxy.cpu().numpy().tolist()[0][3],
-                    "width": box.xyxy.cpu().numpy().tolist()[0][2] - box.xyxy.cpu().numpy().tolist()[0][0],
-                    "height": box.xyxy.cpu().numpy().tolist()[0][3] - box.xyxy.cpu().numpy().tolist()[0][1],
+                    "width": box.xyxy.cpu().numpy().tolist()[0][2]
+                    - box.xyxy.cpu().numpy().tolist()[0][0],
+                    "height": box.xyxy.cpu().numpy().tolist()[0][3]
+                    - box.xyxy.cpu().numpy().tolist()[0][1],
                     "confidence": box.conf.cpu().numpy().tolist()[0],
-                    "class_number": box.cls.cpu().numpy().tolist()[0]
+                    "class_number": box.cls.cpu().numpy().tolist()[0],
                 }
                 box_info["class_name"] = self.model.names[box_info["class_number"]]
                 results_dict["boxes"].append(box_info)

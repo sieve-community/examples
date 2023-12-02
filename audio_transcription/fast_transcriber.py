b022304d8e957aa84d6f8f47bc0b58f8d7e6dd13
Mokshith Voodarla <mvoodarla@gmail.com>
2023-12-02 13:04:29 -0800

    feat: add diarization support to transcriber

--- audio_transcription/fast_transcriber.py
+++ audio_transcription/fast_transcriber.py
@@ -98,18 +98,24 @@ whisper_to_seamless_languages = {
 def audio_split_by_silence(
     file: sieve.File,
     word_level_timestamps: bool = True,
+    speaker_diarization: bool = False,
     speed_boost: bool = False,
     source_language: str = "",
     target_language: str = "",
+    min_speakers: int = -1,
+    max_speakers: int = -1,
     min_silence_length: float = 0.8,
     min_segment_length: float = -1
 ):
     '''
     :param file: Audio file
     :param word_level_timestamps: Whether to return word-level timestamps. Defaults to True.
+    :param speaker_diarization: Whether to perform speaker diarization. Defaults to False.
     :param speed_boost: Whether to use a faster, smaller transcription model. Defaults to False.
     :param source_language: Language of the audio. Defaults to auto-detect if not specified. Otherwise, specify the language code {en, fr, de, es, it, ja, zh, nl, uk, pt}. This may improve transcription speed.
     :param target_language: Language code of the language to translate to (doesn't translate if left blank). See README for supported language codes.
+    :param min_speakers: Minimum number of speakers to detect for diarization. Defaults to auto-detect when set to -1.
+    :param max_speakers: Maximum number of speakers to detect for diarization. Defaults to auto-detect when set to -1.
     :param min_silence_length: Minimum length of silence in seconds to use for splitting audio for parallel processing. Defaults to 0.8.
     :param min_segment_length: Minimum length of audio segment in seconds to use for splitting audio for parallel processing. Defaults to audio length / 20 if set to -1.
     '''
@@ -120,6 +126,16 @@ def audio_split_by_silence(
     import soundfile as sf
     import requests
     import subprocess
+
+    # Do diarization if specified
+    if speaker_diarization:
+        pyannote = sieve.function.get("sieve/pyannote-diarization")
+        diarization_job = pyannote.push(
+            sieve.File(path=file.path),
+            min_speakers=min_speakers,
+            max_speakers=max_speakers,
+        )
+        print("Warning: because speaker diarization is enabled, the transcription output will only return at the end of the job rather than when each segment is finished processing.")
 
     # Extract the length of the audio using ffprobe
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", file.path], capture_output=True, text=True)
@@ -134,7 +150,6 @@ def audio_split_by_silence(
     def split_silences(
         path: str, min_segment_length: float = 30.0, min_silence_length: float = 0.8
     ):
-        import concurrent.futures
         import ffmpeg
 
         silence_end_re = re.compile(
@@ -212,6 +227,7 @@ def audio_split_by_silence(
     if not segments:
         segments.append(whisperx.push(sieve.File(path=file.path)))
 
+    job_outputs = []
     for job in executor.map(process_segment, segments):
         job_output = job.result()
         job_segments = job_output["segments"]
@@ -248,6 +264,26 @@ def audio_split_by_silence(
             modified_job_output["translated_language_code"] = target_language
             modified_job_output["segments"] = job_output["segments"]
             job_output = modified_job_output
-        yield job_output
+        
+        job_outputs.append(job_output)
+        if not speaker_diarization:
+            yield job_output
+    
+    if speaker_diarization:
+        diarization_job_output = diarization_job.result()
+        print("diarization finished")
+        for job_output in job_outputs:
+            transcript_segments = job_output["segments"]
+            for seg in transcript_segments:
+                diarization_segments = [s for s in diarization_job_output if s['end'] >= seg['start'] and s['start'] <= seg['end']]
+                if diarization_segments:
+                    seg["speaker"] = max(diarization_segments, key=lambda x: x['end'] - x['start'])['speaker_id']
+                if 'words' in seg:
+                    for word in seg['words']:
+                        if 'start' in word:
+                            diarization_segments = [s for s in diarization_job_output if s['end'] >= word['start'] and s['start'] <= word['end']]
+                            if diarization_segments:
+                                word["speaker"] = max(diarization_segments, key=lambda x: x['end'] - x['start'])['speaker_id']
+            yield job_output
     
     print("transcription finished")

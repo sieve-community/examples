cb336c4f084ba4783323bc26bef489ca27e02ab0
Mokshith Voodarla <mvoodarla@gmail.com>
2024-04-25 12:38:28 -0700

    feat: pick default language variants

--- translation/translate/deepl_translator.py
+++ translation/translate/deepl_translator.py
@@ -6,6 +6,10 @@ def get_deepl_translation(text: str, source_language: str, target_language: str)
     if not deepl_api_key:
         raise ValueError("DEEPL_API_KEY environment variable not set")
     translator = deepl.Translator(deepl_api_key)
+    if target_language == 'en':
+        target_language = 'EN-US'
+    if target_language == 'pt':
+        target_language = 'PT-PT'
     result = translator.translate_text(text, source_lang=source_language, target_lang=target_language)
     return result.text
 

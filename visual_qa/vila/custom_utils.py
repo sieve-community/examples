e3466df8682c1a392d4505add80eb775d382dac2
Gaurang Bharti <gaurangbharti@gmail.com>
2024-05-13 15:37:04 +0000

    Use Decord in VILA

--- visual_qa/vila/custom_utils.py
+++ visual_qa/vila/custom_utils.py
@@ -1,46 +1,11 @@
 import cv2
 from PIL import Image
-from io import BytesIO
-import tempfile
 import numpy as np
 
-def opencv_extract_frames(vpath_or_bytesio, sampling_count=6, fps=None, frame_count=None, sampling_strategy="auto"):
-    """
-    Extract frames from a video using OpenCV.
 
-    Args:
-        vpath_or_bytesio (str or BytesIO): Path to the video file or BytesIO object containing the video.
-        frames (int): Number of frames to extract from the video.
-        fps (int): Frames per second of the video. If None, it will be inferred from the video.
-        frame_count (int): Number of frames in the video. If None, it will be inferred from the video.
-
-
-    Returns:
-        list: List of PIL Images extracted from the video.
-
-    Raises:
-        NotImplementedError: If the type of `vpath_or_bytesio` is not supported.
-    """
-    import cv2
-
-    if isinstance(vpath_or_bytesio, str):
-        vidcap = cv2.VideoCapture(vpath_or_bytesio)
-        return get_frame_from_vcap(vidcap, sampling_count, fps=fps, frame_count=frame_count, sampling_strategy=sampling_strategy)
-    elif isinstance(vpath_or_bytesio, (BytesIO,)):
-        # assuming mp4
-        with tempfile.NamedTemporaryFile(delete=True, suffix=".mp4") as temp_video:
-            temp_video.write(vpath_or_bytesio.read())
-            temp_video_name = temp_video.name
-            vidcap = cv2.VideoCapture(temp_video_name)
-            return get_frame_from_vcap(vidcap, sampling_count, fps=fps, frame_count=frame_count, sampling_strategy=sampling_strategy)
-    else:
-        raise NotImplementedError(type(vpath_or_bytesio))
-
-
-def get_frame_from_vcap(vidcap, num_frames=10, fps=None, frame_count=None, sampling_strategy="auto"):
-    if fps is None or frame_count is None:
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+def get_frame_from_vcap(vidcap, num_frames=10, sampling_strategy="auto"):
+    fps = vidcap.get_avg_fps()
+    frame_count = len(vidcap)
     
     if fps == 0 or frame_count == 0:
         print("Video file not found. Return empty images.")
@@ -57,9 +22,8 @@ def get_frame_from_vcap(vidcap, num_frames=10, fps=None, frame_count=None, sampl
     num_frames = len(frame_indices)
 
     for frame_index in frame_indices:
-        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        success, frame = vidcap.read()
-        if success:
+        frame = vidcap[frame_index].asnumpy()
+        if frame is not None:
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             im_pil = Image.fromarray(img)
             images.append(im_pil)
@@ -70,4 +34,3 @@ def get_frame_from_vcap(vidcap, num_frames=10, fps=None, frame_count=None, sampl
         print("Did not find enough valid frames, returning what was collected.")
 
     return images
-

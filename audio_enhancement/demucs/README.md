7f89a5ce99a4de0b711285693f8530cfbcb4d762
Ahmed Hanzala <105395393+ahmedHanzala@users.noreply.github.com>
2024-05-20 00:36:52 +0500

    adding demucs

---
+++ audio_enhancement/demucs/README.md
@@ -0,0 +1,25 @@
+# Demucs Source Seperation
+
+Demucs is a state-of-the-art music source separation model, currently capable of separating drums, bass, and vocals from the rest of the accompaniment. Demucs is based on a U-Net convolutional architecture inspired by Wave-U-Net. The v4 version features Hybrid Transformer Demucs, a hybrid spectrogram/waveform separation model using Transformers. It is based on Hybrid Demucs (also provided in this repo), with the innermost layers replaced by a cross-domain Transformer Encoder. This Transformer uses self-attention within each domain, and cross-attention across domains. The model achieves a SDR of 9.00 dB on the MUSDB HQ test set. Moreover, when using sparse attention kernels to extend its receptive field and per source fine-tuning, we achieve state-of-the-art 9.20 dB of SDR.
+
+
+## Usage
+
+To use this function, you need to call the `demucs` function with the sieve library with following parameters:
+
+* `audio` -  Audio file (mp3,wav,flac) to be seperated
+* `model` - The model to be used for audio separation. Default is "htdemucs_ft". Available models are given below
+* `overlap` -  option controls the amount of overlap between prediction windows. Default is 0.25 (i.e. 25%) which is probably fine. It can probably be reduced to 0.1 to improve a bit speed.
+* `shifts` - The number of shifts to be used. performs multiple predictions with random shifts (a.k.a the shift trick) of the input and average them. This makes prediction SHIFTS times slower. Default is 0
+* `mp3` - If True, the audio will be saved as mp3. Default is False
+* `Returns` - A background audio and a foreground audio.
+
+
+## Available Models and details
+- **`htdemucs`**: First version of Hybrid Transformer Demucs. Trained on MusDB + 800 songs. Default model.
+- **`htdemucs_ft`**: Fine-tuned version of `htdemucs`, separation will take 4 times more time but might be a bit better. Same training set as `htdemucs`.
+- **`htdemucs_6s`**: 6 sources version of `htdemucs`, with piano and guitar being added as sources. Note that the piano source is not working great at the moment.
+- **`hdemucs_mmi`**: Hybrid Demucs v3, retrained on MusDB + 800 songs.
+- **`mdx`**: Trained only on MusDB HQ, winning model on track A at the MDX challenge.
+- **`mdx_extra`**: Trained with extra training data (including MusDB test set), ranked 2nd on the track B of the MDX challenge.
+- **`mdx_q`, `mdx_extra_q`**: Quantized version of the previous models. Smaller download and storage but quality can be slightly worse.
---
+++ audio_enhancement/demucs/main.py
@@ -0,0 +1,103 @@
+import sieve
+
+metadata = sieve.Metadata(
+    title="Demucs - Source Seperation",
+    description="Seperate audio sources. This function uses demucs to seperate vocals from non-vocals in an audio. Works best for removing background music.",
+    tags=["Audio", "Speech"],
+    image=sieve.Image(
+        url="https://github.com/adefossez/demucs/blob/main/demucs.png?raw=true"
+    ),
+    readme=open("README.md", "r").read(),
+)
+
+
+@sieve.function(
+    name="demucs",
+    system_packages=["ffmpeg","soundstretch"],
+    python_packages=["git+https://github.com/facebookresearch/demucs#egg=demucs","torch==2.0.1","diffq"],
+    metadata=metadata,
+    gpu=sieve.gpu.L4(),
+    run_commands = [
+        #running models once to load them
+        "python3 -m demucs --two-stems=vocals -n mdx_extra 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n htdemucs 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n htdemucs_ft 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n htdemucs_6s 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n hdemucs_mmi 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n mdx 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n mdx_q 'test.wav'",
+        "python3 -m demucs --two-stems=vocals -n mdx_extra_q 'test.wav'",
+        
+    ],
+    cuda_version="11.8.0"
+
+    )
+def audio_seperator(
+
+    file: sieve.File,
+    model : str = "htdemucs_ft",
+    overlap: float = 0.25,
+    shifts: int = 0,
+    mp3: bool = False,
+    ):
+    """
+    :param audio: Audio file to be separated
+    :param model: The model to be used for audio separation. Default is "htdemucs_ft". Check the README for more information on the available models.
+    :param overlap: option controls the amount of overlap between prediction windows. Default is 0.25 (i.e. 25%) which is probably fine. It can probably be reduced to 0.1 to improve a bit speed.
+    :param shifts: The number of shifts to be used. performs multiple predictions with random shifts (a.k.a the shift trick) of the input and average them. This makes prediction SHIFTS times slower. Default is 0
+    :param mp3: If True, the audio will be saved as mp3. Default is False
+    :return: The separated audio files
+    """
+    import os
+    import demucs.separate
+    import subprocess
+
+    model = model.lower().strip()
+    if model not in ["htdemucs","htdemucs_ft","htdemucs_6s","hdemucs_mmi","mdx","mdx_extra","mdx_q","mdx_extra_q"]:
+        raise Exception("Invalid model selected. Please select from the available models")
+
+    if shifts > 5 or shifts < 0:
+        raise Exception("Invalid shift value. Please select a value between 0 and 5")
+
+    if overlap > 1 or overlap < 0:
+        raise Exception("Invalid overlap value. Please select a value between 0 and 1")
+    
+
+    audio_path = "input_audio" +".wav"
+
+    if file.path.endswith(".mp4"):
+        print("Extracting audio from video")
+        try:
+            subprocess.run(["ffmpeg", "-i", file.path, audio_path, "-y"])
+        except Exception as e:
+            raise Exception("Failed to extract audio from video. Make sure video has audio.")
+    else:
+        audio_path = file.path
+
+    if not os.path.isfile(audio_path):
+        raise Exception("Failed to extract audio!")
+
+    file_name = os.path.splitext(os.path.basename(audio_path))[0]
+    file_extension = 'mp3' if mp3 else 'wav'
+
+
+    print("file name: ", file_name)
+    
+    if mp3:
+        demucs.separate.main(["--mp3","--two-stems", "vocals","--overlap",str(overlap),"--shifts",str(shifts),"-j","16", "-n", model, audio_path])
+    else:
+        demucs.separate.main(["--two-stems", "vocals","--overlap", str(overlap),"--shifts",str(shifts),"-j","16", "-n", model, audio_path])
+
+    vocals = sieve.Audio(path = os.getcwd() + f"/separated/{model}/{file_name}/vocals.{file_extension}")
+    no_vocals = sieve.Audio(path = os.getcwd() + f"/separated/{model}/{file_name}/no_vocals.{file_extension}")
+
+    return vocals, no_vocals
+
+
+if __name__ == "__main__":
+    file = sieve.File(path = "test.wav")
+    output = audio_seperator(
+        file,
+        )
+    print(output)
+

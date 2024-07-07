3c0eb34c7cd88a0f022705d14d1c81310452444b
Mokshith Voodarla <mvoodarla@gmail.com>
2024-07-06 18:11:28 -0700

    feat (youtube download): improve

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -16,8 +16,15 @@ metadata = sieve.Metadata(
     readme=open("README.md", "r").read(),
 )
 
+def merge_audio(video_with_audio, new_audio, output_video):
+    import subprocess
+    # Combine the new audio with the video
+    merge_cmd = f"ffmpeg -y -i '{video_with_audio}' -i '{new_audio}' -c:v copy -map 0:v:0 -map 1:a:0 -shortest '{output_video}'"
+    subprocess.call(merge_cmd, shell=True)
+
 @sieve.function(
     name="youtube_to_mp4",
+    system_packages=["ffmpeg"],
     python_packages=[
         "pytube @ git+https://github.com/sieve-community/pytube"
     ],
@@ -26,27 +33,43 @@ metadata = sieve.Metadata(
 def download(url: str, include_audio: bool = True):
     '''
     :param url: YouTube URL to download
-    :param include_audio: whether or not to ensure the audio is included in the video. if set to false, audio may still be included on occassion.
+    :param include_audio: Whether to include audio in the video.
     :return: The downloaded YouTube video
     '''
     from pytube import YouTube
     import os
 
-    output_filename = "output.mp4"
+    video_filename = "video.mp4"
+    audio_filename = "audio.mp3"
     
-    if os.path.exists(output_filename):
-        print("deleting temp file...")
-        os.remove(output_filename)
+    if os.path.exists(video_filename):
+        print("deleting temp video file...")
+        os.remove(video_filename)
+    
+    if os.path.exists(audio_filename):
+        print("deleting temp audio file...")
+        os.remove(audio_filename)
 
     print("setting stream...")
     yt = YouTube(url)
     yt.register_on_progress_callback(on_progress)
+
     print("filtering stream for highest quality mp4...")
-    streams = yt.streams.filter(progressive=include_audio, file_extension='mp4').order_by('resolution').desc()
-    highest_res_stream = streams.first()
-
+    video = yt.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
     print('downloading video...')
-    highest_res_stream.download(filename=output_filename)
+    video.download(filename=video_filename)
 
-    print("finished downloading video, returning...")
-    return sieve.File(path=output_filename)
+    if include_audio:
+        print('downloading audio...')
+        audios = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        audios.download(filename=audio_filename)
+        print('merging audio...')
+        merge_audio(video_filename, audio_filename, "output.mp4")
+        print('Done!')
+        return sieve.File(path="output.mp4")
+    else:
+        print('Done!')
+        return sieve.File(path=video_filename)
+
+if __name__ == "__main__":
+    download("https://www.youtube.com/watch?v=AKJfakEsgy0", include_audio=True)
